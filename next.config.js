bd1a70f2195632531ffe71590d65d2c0619eab14
xiaochu <chulingli1988@outlook.com>
2026-09-20 09:09:13 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['t.me', 'oauth.safew.bot'],
+  },
+};
+
+module.exports = nextConfig;
