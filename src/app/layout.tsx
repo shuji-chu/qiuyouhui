ee1d2057af30d4c7026f9720431a16df731d84b8
xiaochu <chulingli1988@outlook.com>
2026-09-20 09:49:52 +0800

    Change themeColor to a lighter shade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  themeColor: '#0a0a0a',
+  themeColor: '#dbeafe',
 };
 
 export default function RootLayout({
